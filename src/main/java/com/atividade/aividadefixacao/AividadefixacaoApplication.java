d9c07edd8eab8bc7cd848886a1070777bb55fb31
Matheus Becker <beckerm@weg.net>
2024-04-04 14:17:27 -0300

    first commit

---
+++ src/main/java/com/atividade/aividadefixacao/AividadefixacaoApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.aividadefixacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AividadefixacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AividadefixacaoApplication.class, args);
+	}
+
+}
